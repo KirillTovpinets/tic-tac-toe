17aa9cca001f97c964398eb3087979e5501b0eb5
Kirill Tovpinec <kirilltovpinec@KirillTovpinets-Macbook-Air.local>
2019-05-10 15:53:58 +0300

    Updates

--- precache-manifest.cd2258ca80f4a844ed3491a9670360dc.js
+++ precache-manifest.cd2258ca80f4a844ed3491a9670360dc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "93673ee5c95fd1001f3fe8c4a1dfd1eb",
+    "revision": "df46994097207f90ec7efd5d458fbb87",
     "url": "/tic-tac-toe/index.html"
   },
   {
-    "revision": "22261497a61a3c98c7e7",
+    "revision": "a3ac482a62190662991c",
     "url": "/tic-tac-toe/static/css/main.655fac19.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tic-tac-toe/static/js/2.5e4f76da.chunk.js"
   },
   {
-    "revision": "22261497a61a3c98c7e7",
-    "url": "/tic-tac-toe/static/js/main.c1c46d39.chunk.js"
+    "revision": "a3ac482a62190662991c",
+    "url": "/tic-tac-toe/static/js/main.0ffd2f24.chunk.js"
   },
   {
     "revision": "9728fa41a930f12fdc87",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe/precache-manifest.7e67133307d3e75fe1b431fa1f0519a9.js"
+  "/tic-tac-toe/precache-manifest.cd2258ca80f4a844ed3491a9670360dc.js"
 );
 
 self.addEventListener('message', (event) => {
